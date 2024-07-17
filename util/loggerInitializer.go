e12531a0949a21daa611d1bd410942d19e2df85d
kushal chapagain <chapagainkushal74@gmail.com>
2024-07-17 22:31:10 +0545

    change .txt to .log

--- util/loggerInitializer.go
+++ util/loggerInitializer.go
@@ -8,7 +8,7 @@ import (
 )
 
 func InitializeLogger() *log.Logger {
-	fileName := fmt.Sprintf("logfiles/info_%s.txt", time.Now().Format("2006-01-02"))
+	fileName := fmt.Sprintf("logfiles/info_%s.log", time.Now().Format("2006-01-02"))
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0700)
 	if err != nil {
 		log.Fatal("error while creating log files")
