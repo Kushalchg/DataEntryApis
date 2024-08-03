dc3bc6af85f5cbca97ee6b70aee006b7844d5144
kushal chapagain <chapagainkushal74@gmail.com>
2024-08-03 21:46:37 +0545

    fixed

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func init() {
 	global.Validate = validator.New(validator.WithRequiredStructEnabled())
-	initializers.LoadEnv()
+	// initializers.LoadEnv()
 	initializers.Connectdb()
 
 }
