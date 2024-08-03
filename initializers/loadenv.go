593f2900d81f7b0ddaeaef84d459ba0d38035b7c
kushal chapagain <chapagainkushal74@gmail.com>
2024-08-03 21:46:00 +0545

    fixed

--- initializers/loadenv.go
+++ initializers/loadenv.go
@@ -1,15 +1,15 @@
 package initializers
 
-import (
-	"log"
-	"path/filepath"
+// import (
+// 	"log"
+// 	"path/filepath"
 
-	"github.com/joho/godotenv"
-)
+// 	"github.com/joho/godotenv"
+// )
 
-func LoadEnv() {
-	err := godotenv.Load(filepath.Join("../", ".env"))
-	if err != nil {
-		log.Fatal("error while loading env variables")
-	}
-}
+// func LoadEnv() {
+// 	err := godotenv.Load(filepath.Join("../", ".env"))
+// 	if err != nil {
+// 		log.Fatal("error while loading env variables")
+// 	}
+// }
